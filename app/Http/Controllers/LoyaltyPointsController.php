73a476cf149cdf8a4de038312e4085680d040a31
Alex <lg280683man1@yandex.ru>
2024-10-06 11:25:09 +0300

    Добавление сервиса для контроллера LoyaltyPointsController.php  - зарегистрировал сервис провайдер LoyaltyPointsServiceProvider.php  - добавил сервис LoyaltyPointsService.php  - добавил интерфейс LoyaltyPointsServiceInterface.php  - добавил сервис в конструктор контроллера LoyaltyPointsController.php

--- app/Http/Controllers/LoyaltyPointsController.php
+++ app/Http/Controllers/LoyaltyPointsController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers;
 use App\Mail\LoyaltyPointsReceived;
 use App\Models\LoyaltyAccount;
 use App\Models\LoyaltyPointsTransaction;
+use App\Services\LoyaltyPointsServiceInterface;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class LoyaltyPointsController extends Controller
 {
+    private LoyaltyPointsServiceInterface $loyaltyPointsService;
+    public function __construct(LoyaltyPointsServiceInterface $loyaltyPointsService)
+    {
+        $this->loyaltyPointsService = $loyaltyPointsService;
+    }
+
     public function deposit()
     {
         $data = $_POST;
